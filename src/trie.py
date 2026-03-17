d63931c7d0b5e06ea88e595760fd5bf8aeeda9a6
heikkilotjonen <heikkimlotjonen@gmail.com>
2026-03-17 14:44:49 +0200

    Added initial distance algorithm and trie-tree config

---
+++ src/trie.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode() 
+
+    def insert(self, word):                         # Lisää sana trie:hen
+        node = self.root
+        for char in word:                           # Käy läpi sanan merkit
+            if char not in node.children:           # Jos merkki ei ole lapsissa, luodaan uusi solmu
+                node.children[char] = TrieNode()
+            node = node.children[char]              # Siirrytään seuraavaan solmuun
+        node.is_word = True
+
+    def search(self, word):                         # Tarkistaa, onko trie:ssä sana
+        node = self.root
+        for char in word:                           # Käy läpi sanan merkit
+            if char not in node.children:           # Jos merkki ei ole lapsissa, palautetaan False
+                return False
+            node = node.children[char]              # Siirrytään seuraavaan solmuun
+        return node.is_word
+
+    def starts_with(self, prefix):                  # Tarkistaa, onko trie:ssä sana, joka alkaa etuliitteellä
+        node = self.root
+        for char in prefix:                         # Käy läpi etuliitteen merkit
+            if char not in node.children:           # Jos merkki ei ole lapsissa, palautetaan False
+                return False    
+            node = node.children[char]              # Siirrytään seuraavaan solmuun
+        return True
